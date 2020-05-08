0c9b4aa49e192528caefb54f79aafddbf56e699c
julian@soluciones404.com.co <PAJA__R1T02>
2020-05-08 18:12:58 -0500

    - Re organizing checkout elements

--- js/source/main.js
+++ js/source/main.js
@@ -6,9 +6,12 @@ const editPage = () => {
     const titleElement = $(item).find('h4');
     const link = $(titleElement).find('a').attr('href');
     if (link) {
-      titleElement.after(`<a href="${link}">${LINK_TITLE}</a>`);
+      titleElement.after(`<a class="edit-item-link" href="${link}">${LINK_TITLE}</a>`);
     }
   });
+  // Donations
+  $('div[id*="_divSummaryOuter"]').after($('div[id*="_additionalDonationSection"]'));
+  $('div[id*="_divPersonalInfo"]').after($('#divCartSummary .form-group.lead.text-success'));
 };
 
 editPage();
