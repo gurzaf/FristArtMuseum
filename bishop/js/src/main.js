036bcaa9dfd40eba6e213807933d05dc0d30a505
julian@soluciones404.com.co <PAJA__R1T02>
2020-06-25 14:25:30 -0500

    - Copy on General Admission changed. - Removed translation menu

--- bishop/js/src/main.js
+++ bishop/js/src/main.js
@@ -1,8 +1,8 @@
-function googleTranslateElementInit2() {
-  new google.translate.TranslateElement({pageLanguage: 'en',autoDisplay: false}, 'google_translate_element2');
-}
+// function googleTranslateElementInit2() {
+//   new google.translate.TranslateElement({pageLanguage: 'en',autoDisplay: false}, 'google_translate_element2');
+// }
 
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}',43,43,'||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'),0,{}));
+// eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}',43,43,'||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'),0,{}));
 
 (() => {
   const LINK_TITLE = 'Review or edit item';
@@ -250,63 +250,63 @@ eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
     });
   };
 
-  const checkLanguage = () => {
-    const current = readCookie('googtrans');
-    let selector = '#translate-menu a';
-    $(selector).removeClass('active');
-    if (current === '/en/ja') {
-      selector = `${selector}:last-child`;
-    } else {
-      selector = `${selector}:first-child`;
-    }
-    $(selector).addClass('active');
-  };
+  // const checkLanguage = () => {
+  //   const current = readCookie('googtrans');
+  //   let selector = '#translate-menu a';
+  //   $(selector).removeClass('active');
+  //   if (current === '/en/ja') {
+  //     selector = `${selector}:last-child`;
+  //   } else {
+  //     selector = `${selector}:first-child`;
+  //   }
+  //   $(selector).addClass('active');
+  // };
   
-  const translationOptions = () => {
-    // It also includes main menu
-    // $('.MS_LoginButtonOuterWrapperContainer .MS_LoginButtonInnerContainer').after(`
-    //   <div id="new-menu-links">
-    //       <div class="container">
-    //         <div class="row">
-    //           <div class="col col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-    //             <ul class="main-menu">
-    //               <li><a aria-label="Go to visit" href="https://www.bishopmuseum.org/visit/">Visit</a></li>
-    //               <li><a aria-label="Go to Exhibits & Programs" href="https://www.bishopmuseum.org/exhibits-and-programs/">Exhibits & Programs</a></li>
-    //               <li><a aria-label="Go to Explore" href="https://www.bishopmuseum.org/explore/">Explore</a></li>
-    //               <li><a aria-label="Go to Education" href="https://www.bishopmuseum.org/education/">Education</a></li>
-    //               <li><a aria-label="Go to Join & Give" href="https://www.bishopmuseum.org/membership/">Join & Give</a></li>
-    //             </ul>
-    //           </div>
-    //         </div>
-    //       </div>
-    //   </div>
-    //   <div id="translate-menu">
-    //     <a href="#" onclick="doGTranslate('en|en');" title="English" class="skiptranslate active">
-    //       English
-    //     </a>
-    //     <a href="#" onclick="doGTranslate('en|ja');" title="日本語" class="skiptranslate">
-    //       日本語
-    //     </a>
-    //   </div>
-    // `);
+  // const translationOptions = () => {
+  //   // It also includes main menu
+  //   // $('.MS_LoginButtonOuterWrapperContainer .MS_LoginButtonInnerContainer').after(`
+  //   //   <div id="new-menu-links">
+  //   //       <div class="container">
+  //   //         <div class="row">
+  //   //           <div class="col col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+  //   //             <ul class="main-menu">
+  //   //               <li><a aria-label="Go to visit" href="https://www.bishopmuseum.org/visit/">Visit</a></li>
+  //   //               <li><a aria-label="Go to Exhibits & Programs" href="https://www.bishopmuseum.org/exhibits-and-programs/">Exhibits & Programs</a></li>
+  //   //               <li><a aria-label="Go to Explore" href="https://www.bishopmuseum.org/explore/">Explore</a></li>
+  //   //               <li><a aria-label="Go to Education" href="https://www.bishopmuseum.org/education/">Education</a></li>
+  //   //               <li><a aria-label="Go to Join & Give" href="https://www.bishopmuseum.org/membership/">Join & Give</a></li>
+  //   //             </ul>
+  //   //           </div>
+  //   //         </div>
+  //   //       </div>
+  //   //   </div>
+  //   //   <div id="translate-menu">
+  //   //     <a href="#" onclick="doGTranslate('en|en');" title="English" class="skiptranslate active">
+  //   //       English
+  //   //     </a>
+  //   //     <a href="#" onclick="doGTranslate('en|ja');" title="日本語" class="skiptranslate">
+  //   //       日本語
+  //   //     </a>
+  //   //   </div>
+  //   // `);
 
-    // $('form#form1').before('<div id="google_translate_element2"></div>');
+  //   // $('form#form1').before('<div id="google_translate_element2"></div>');
 
-    const dynamicallyLoadScript = (url) => {
-      var script = document.createElement('script');  // create a script DOM node
-      script.src = url;  // set its src to the provided URL
-      document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
-    };
+  //   const dynamicallyLoadScript = (url) => {
+  //     var script = document.createElement('script');  // create a script DOM node
+  //     script.src = url;  // set its src to the provided URL
+  //     document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+  //   };
 
-    dynamicallyLoadScript('https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2');
+  //   dynamicallyLoadScript('https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2');
 
-    checkLanguage();
+  //   checkLanguage();
 
-    $('#translate-menu a').click((e) => {
-      checkLanguage();
-      e.preventDefault();
-    });
-  };
+  //   $('#translate-menu a').click((e) => {
+  //     checkLanguage();
+  //     e.preventDefault();
+  //   });
+  // };
 
   const passTextValue = (from, to) => {
     $(from).on('keyup', () => {
@@ -347,7 +347,6 @@ eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
       || $('[aria-describedby$="UserModalSignIn_UserModalPartDialog1"]').css('display') === 'block') {
       return;
     }
-    console.log('Creating new popup');
     const basicHtml = `
       <div style="display: none;">
         <div id="popuplogin" class="container ui-corner-all">
@@ -400,17 +399,12 @@ eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
       </div>
     `;
     $('[aria-describedby*="UserModalSignIn_UserModalPartDialog1"]').appendTo('form');
-    console.log('Append dialog to form');
     $(basicHtml).appendTo('form');
-    console.log('Attaching new dialog');
     $('#new-submit').on('click', newLoginSubmit);
-    console.log('New submit click event');
     passTextValue('#new-username', '[id$="UserModalSignIn_UserModalPartDialog1_UserModalPartDialogBody_TextboxUserName"]');
     passTextValue('#new-password', '[id$="UserModalSignIn_UserModalPartDialog1_UserModalPartDialogBody_TextboxPassword"]');
     passClick('#new-checkbox', '[id$="UserModalSignIn_UserModalPartDialog1_UserModalPartDialogBody_CheckboxRememberSignIn"]');
-    console.log('Passing events');
     window.scrollTo(0, 0);
-    console.log('Trying to create a new dialog');
     $('#popuplogin').dialog({
       title: null,
       position: { my: "top", at: "center", of: ".site-header.row" },
@@ -458,7 +452,7 @@ eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
         replaceText();
         organiceItems();
         editCurrentLoginPopup();
-        translationOptions();
+        // translationOptions();
         createPopUp();
         setFavicon();
       });
