76ef39b8df3fdbf8d0f2bdb478c038d618b532fa
julian@soluciones404.com.co <PAJA__R1T02>
2020-07-27 00:43:55 -0500

    - Replace header logo - Replace regex to remove user stylesheet

--- frist/js/source/main.js
+++ frist/js/source/main.js
@@ -2,7 +2,7 @@
   const init = () => {
     const cdn = 'https://dev.juliangalvez.xyz/frist';
     // const cdn = '.';
-    $('link[rel=stylesheet][href*="Webforms-User-Stylesheet.css"]').remove();
+    $('link[rel=stylesheet][href*="Webforms-User-Stylesheet"]').remove();
     $('.MSFootTextDiv').html('');
     $('.MS_LoginButtonOuterWrapperContainer').hide();
     $('.MSFootTextDiv:first').load(`${cdn}/html/header.html`, () => {
