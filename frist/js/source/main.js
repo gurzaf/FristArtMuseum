ef560987ab5222c23ce35cac952764fa931c9cf8
julian@soluciones404.com.co <PAJA__R1T02>
2020-07-27 00:37:56 -0500

    Removing dynamically Altru user style

--- frist/js/source/main.js
+++ frist/js/source/main.js
@@ -1,6 +1,8 @@
 (() => {
   const init = () => {
     const cdn = 'https://dev.juliangalvez.xyz/frist';
+    // const cdn = '.';
+    $('link[rel=stylesheet][href*="Webforms-User-Stylesheet.css"]').remove();
     $('.MSFootTextDiv').html('');
     $('.MS_LoginButtonOuterWrapperContainer').hide();
     $('.MSFootTextDiv:first').load(`${cdn}/html/header.html`, () => {
