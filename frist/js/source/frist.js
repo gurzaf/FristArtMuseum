3edd584fff456dc2ad71067523d2d83dbf0449cd
julian@soluciones404.com.co <PAJA__R1T02>
2020-07-27 01:52:39 -0500

    New way to load Google Translate

--- frist/js/source/frist.js
+++ frist/js/source/frist.js
@@ -143,8 +143,49 @@
     `);
     dynamicallyLoadScript('https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2', null);
     dynamicallyLoadScript(null, `
-      /* <![CDATA[ */
-        eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}',43,43,'||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'),0,{}))
+    /* <![CDATA[ */
+      function GTranslateFireEvent(element, event) {
+        try {
+          if (document.createEventObject) {
+            var evt = document.createEventObject();
+            element.fireEvent('on' + event, evt)
+          } else {
+            var evt = document.createEvent('HTMLEvents');
+            evt.initEvent(event, true, true);
+            element.dispatchEvent(evt)
+          }
+        } catch (e) {}
+      }
+    
+      function doGTranslate(lang_pair) {
+        if (lang_pair.value) lang_pair = lang_pair.value;
+        if (lang_pair == '') return;
+        var lang = lang_pair.split('|')[1];
+        var teCombo;
+        var sel = document.getElementsByTagName('select');
+        for (var i = 0; i < sel.length; i++)
+          if (sel[i].className == 'goog-te-combo') teCombo = sel[i];
+        if (document.getElementById('google_translate_element2') == null ||
+            document.getElementById('google_translate_element2').innerHTML.length == 0 || 
+            teCombo.length == 0 || 
+            teCombo.innerHTML.length == 0) {
+          setTimeout(function() {
+            doGTranslate(lang_pair)
+          }, 500)
+        } else {
+          teCombo.value = lang;
+          GTranslateFireEvent(teCombo, 'change');
+          GTranslateFireEvent(teCombo, 'change')
+        }
+      }
+    
+      function GTranslateGetCurrentLang() {
+        var keyValue = document.cookie.match('(^|;) ?googtrans=([^;]*)(;|$)');
+        return keyValue ? keyValue[2].split('/')[2] : null;
+      }
+      if (GTranslateGetCurrentLang() != null) jQuery(document).ready(function() {
+        jQuery('div.switcher div.selected a').html(jQuery('div.switcher div.option').find('span.gflag img[alt="' + GTranslateGetCurrentLang() + '"]').parent().parent().html());
+      });
       /* ]]> */
     `);
   };
